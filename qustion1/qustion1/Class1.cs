402a2ad1bc339323928e865a10c8dd337f79371c
mohamedissam6 <112808440+mohamedissam6@users.noreply.github.com>
2022-09-04 20:43:52 +0200

    Add files via upload

---
+++ qustion1/qustion1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qustion1
+{
+    internal class Rectangle
+    {
+        private float length;
+        private float width;
+       
+        public Rectangle() { length = 1.0f; width = 1.0f; }
+        public Rectangle(float length, float width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public float Length {
+            set {
+                if ((value>0.0) && (value < 20.0))
+                    length = value; 
+            }
+        get {
+                return length; 
+            }
+        }
+        public float Width
+        {
+            get 
+            {
+                return width; 
+             }
+            set
+            {
+                if ((value > 0.0) && (value < 20.0))
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public float Perimeter()
+        {
+            return((2 * (length + width)));
+       }
+        public float Area()
+        {
+           return((length * width));
+        }
+    }
+}
---
+++ qustion1/qustion1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qustion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10,15);
+            Console.WriteLine(rect.Perimeter());
+            Console.WriteLine(rect.Area());
+            
+        }
+    }
+}
